e40b543b78ddbe7ed92f916e2ed9332b7aa8cc61
doc <doc.divxm@gmail.com>
2014-04-13 13:20:55 +0300

    Revert "i9500: add device specific audio effects table"
    This reverts commit d7250591b9e02af66edc17e86165780f8e66626d.
    
    
    Change-Id: I80259559f5f2be09b174590cec7e4207a10bbb77
    Signed-off-by: doc <doc.divxm@gmail.com>

--- device.mk
+++ device.mk
@@ -40,8 +40,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
